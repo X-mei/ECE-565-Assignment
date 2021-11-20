151335420497f8ce540ae9c280e1873aa5d897f2
X-mei <mg476@duke.edu>
2021-11-20 01:43:59 -0500

    1.43

--- hw5/hw5/rainfall/rainfall_pt.cpp
+++ hw5/hw5/rainfall/rainfall_pt.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <cstring>
 #include <pthread.h>
+#include <math.h>
 
 using namespace std;
 
@@ -22,9 +23,12 @@ vector<vector<double>> water_level;
 vector<vector<double>> absorbed;
 vector<vector<int>> grid_height;
 vector<vector<double>> level_change;
+bool done = false;
 
 // parallel
-
+int num_tasks;
+vector<vector<pthread_mutex_t>> lockes;
+pthread_barrier_t barrier_update_water;
 
 // result
 int step_taken = 0;
@@ -43,7 +47,7 @@ double calc_time(struct timeval start, struct timeval end) {
     }
 }
 
-void print_water(double water_level[][4], int N){
+void print_water(){
     cout << "###################" << endl;
     for (int i=0; i<N; ++i){
         for (int j=0; j<N; ++j){
@@ -71,7 +75,7 @@ void init_data(string file_name) {
     }
 }
 
-void precompute() {
+void compute_flowmap() {
     // precompute the flow pattern that will occur at each spot
     vector<pair<int, int>> masks{{-1,0},{1,0},{0,1},{0,-1}};
     for (int i=0; i<N; ++i){
@@ -121,61 +125,69 @@ void print_results(struct timeval start, struct timeval end) {
 
 /* Main compute function */
 void run_one_timestep_point(int i, int j) {
+    // falls and absorbs won't affected by other spot in one step.
     // rain falls
+    // cout << "run point: " << i << ", " << j << endl;
     if (M > 0){
         water_level[i][j] += 1;
     }
     // absorbed by current spot
-    if (water_level[i][j] >= A){
+    if (water_level[i][j] >= A) {
         water_level[i][j] -= A;
         absorbed[i][j] += A;
     }
-    else if (water_level[i][j] > 0){
+    else if (water_level[i][j] > 0) {
         absorbed[i][j] += water_level[i][j];
         water_level[i][j] = 0;
     }
     // flow to other spot
-    if (!flow_map[i][j].empty()){
+    if (!flow_map[i][j].empty()) {
         double split_cnt = flow_map[i][j].size();
         if (water_level[i][j] >= 1){
             water_level[i][j] -= 1;
-            for (auto dest: flow_map[i][j]){
+            for (auto dest: flow_map[i][j]) {
+                pthread_mutex_lock(&lockes[dest.first][dest.second]);
                 level_change[dest.first][dest.second] += 1.0/split_cnt;
+                pthread_mutex_unlock(&lockes[dest.first][dest.second]);
             }
         }
-        else if (water_level[i][j]>0){
-            for (auto dest: flow_map[i][j]){
+        else if (water_level[i][j] > 0){
+            for (auto dest: flow_map[i][j]) {
+                pthread_mutex_lock(&lockes[dest.first][dest.second]);
                 level_change[dest.first][dest.second] += water_level[i][j]/split_cnt;
+                pthread_mutex_unlock(&lockes[dest.first][dest.second]);
             }
             water_level[i][j] = 0;
         }
     }
+    // not all done once water remain in any point
+    if (water_level[i][j] > 0) {
+        done = false;
+    }
 }
 
-void* runSimulation(void* args) {
-    /* divide landscap by row to simultaneous groups */
-    int thrdID = *(int*)args;
+void* run_part_simulation(void* args) {
+    /* divide landscape by row to parallel groups */
+    int thrdID = *(int*)args - 1;
+    cout << "run part" << endl;
+    cout << "thrdID " << thrdID << ", " << thrdID * num_tasks << endl;
 
-    int num_tasks = N / P;
-    bool done = false;
-    while (!done){
-        done = true;
+    while (!done) {
+        done = true;    // assume true first
         level_change = vector<vector<double>>(N, vector<double>(N, 0));
-        for (int i=0; i<N; ++i){
-            for (int j=0; j<N; ++j){
+        for (int i = thrdID * num_tasks; i < min((thrdID + 1)* num_tasks, N); ++i){
+            for (int j = 0; j < N; ++j){
                 run_one_timestep_point(i, j);
             }
         }
-        // this check should wait until one timestep complete
-        for (int i = 0; i < N; ++i){
-            for (int j = 0; j < N; ++j){
-                // determine if all is done
-                if (water_level[i][j] > 0){
-                    done = false;
-                }
-                water_level[i][j] += level_change[i][j];
+        // below check should wait until one whole timestep complete
+        // pthread_barrier_wait(&barrier_update_water);
+        for (int i = thrdID * num_tasks; i < min((thrdID + 1)* num_tasks, N); ++i){
+            for (int j = 0; j < N; ++j) {
+                water_level[i][j] += level_change[i][j];    // add water from other point
             }
         }
+
         step_taken++;
         M--;
     }
@@ -192,19 +204,36 @@ int main(int argc, char * argv[]){
     N = atoi(argv[4]);      // landscape dimension, NxN
     std::string file_name = argv[5];
     init_data(file_name);
+    num_tasks = ceil(float(N) / P);
+    cout << "num tasks " << num_tasks << endl;
+    // init mutexes
+    for (size_t i = 0; i < N; i++) {
+        vector<pthread_mutex_t> line;
+        for (size_t j = 0; j < N; j++) {
+            line.push_back(PTHREAD_MUTEX_INITIALIZER);
+        }
+        lockes.push_back(line);
+    }
+    // init barrier
+    pthread_barrier_init(&barrier_update_water, NULL, P);
 
     /* parallelly run simulation */
     struct timeval start_time, end_time;
     gettimeofday(&start_time, NULL);
-    precompute();
+    compute_flowmap();
 
     // Create the threads
     pthread_t* threads;
     threads = (pthread_t *) malloc(P * sizeof(pthread_t));
     for (int i = 0; i < P; i++) {
-        // use i as the threadID
-        int thrdID = i;
-        pthread_create(&threads[i], NULL, runSimulation, (void*)thrdID);
+        // pass in i as the threadID
+        void* thrdID = (void*)&i;
+        cout << "thrdID " << i << endl;
+        pthread_create(&threads[i], NULL, run_part_simulation, thrdID);
+    }
+
+    for (int i = 0; i < P; i++) {
+        pthread_join(threads[i],NULL);
     }
 
     gettimeofday(&end_time, NULL);
